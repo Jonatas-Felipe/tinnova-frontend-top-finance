0f93ade3ace35164ebcc8f3d15702e2d394adb39
Jonatas Felipe <jonatas-nandes@hotmail.com>
2025-09-05 19:07:59 -0300

    Ajustes

--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig(({ mode }) => ({
       shared: ['react', 'react-dom', 'zustand'],
     }) : federationPlugin({
       name: 'topFinance',
-      filename: 'assets/remoteEntry.js',
+      filename: 'remoteEntry.js',
       exposes: {
         './App': './src/App.tsx',
       },
